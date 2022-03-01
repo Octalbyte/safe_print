df7fa078162dd257f3ee0c25e462b4da66e6c4d6
jp <jpso.guerreiro@gmail.com>
2022-03-01 08:26:39 -0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+
+pub fn safe_print(a1: &str) {
+    print!("{}", a1);
+    io::stdout().flush().unwrap();
+}
+
+
+
+pub fn safe_eprint(a1: &str) {
+    eprint!("{}", a1);
+    io::stderr().flush().unwrap();
+}
